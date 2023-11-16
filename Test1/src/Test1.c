63941f59b117e460c8206f071e9b17d6f6d6631e
Valentino_L <valentino.lew@gmail.com>
2023-11-16 16:32:42 +0100

    Updated sendOverRS232 method

--- Test1/src/Test1.c
+++ Test1/src/Test1.c
@@ -10,22 +10,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
 #include <string.h>
 
-void sendOverRS232(const char *data, const char *rs232Interface) {
+int sendOverRS232(const char *data, const char *rs232Interface) {
 
     printf("Sending data over RS232 interface: %s\n", rs232Interface);
-
-    // Do something
-
     printf("Data sent: %s\n", data);
+
+    const char *port = "/dev/ttyUSB0"; // needs to be changed
+    int serial_port = open(port, O_RDWR | O_NOCTTY);
+
+    if (serial_port < 0) {
+        perror("Error opening serial port");
+        return -1;
+    }
+
+    struct termios portSettings;
+
+    // get current attributes
+    if (tcgetattr(serial_port, &portSettings) != 0) {
+        perror("Error from tcgetattr");
+        close(serial_port);
+        return -1;
+    }
+
+    // baud rate needs to be changed
+    cfsetospeed(&portSettings, B9600);
+    cfsetispeed(&portSettings, B9600);
+
+    // set port settings
+    portSettings.c_cflag |= (CLOCAL | CREAD); // Enable receiver and ignore modem control lines
+    portSettings.c_cflag &= ~CSIZE;
+    portSettings.c_cflag |= CS8;              // 8-bit data
+    portSettings.c_cflag &= ~PARENB;          // No parity bit
+    portSettings.c_cflag &= ~CSTOPB;          // 1 stop bit
+    portSettings.c_cflag &= ~CRTSCTS;         // No hardware flow control
+
+    // set input output mode to raw
+    portSettings.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    portSettings.c_oflag &= ~OPOST;
+
+    // set new attributes
+    if (tcsetattr(serial_port, TCSANOW, &portSettings) != 0) {
+        perror("Error from tcsetattr");
+        close(serial_port);
+        return -1;
+    }
+
+    write(serial_port, data, strlen(data));
+
+    close(serial_port);
+
+    return 0;
+
 }
 
 int main(int argc, char *argv[]) {
 
 	printf("Running! \n");
 
-    // Check if correct number of command line arguments are provided
+    // check if correct number of command line arguments are provided
     if (argc != 3 && argc != 4) {
     	printf("Incorrect command line inputs! \n");
     	printf("gcc XXX -o XXX ./XXX XXX rs232_interface XXX \n");
